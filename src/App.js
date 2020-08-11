d8d5dc9401543944669019347e1efc767d3f23af
Amruta Khot <amrutakhot2994@gmail.com>
2020-08-11 19:15:28 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="grid-container">
       <header>
-        <a href="/">React Shopping</a>
+        <a href="/">Amruta Shopping</a>
       </header>
       <main>Product List</main>
       <footer>All right is reserved.</footer>

ee930900d9650a71c8e4d9610439edbf41441254
Amruta Khot <amrutakhot2994@gmail.com>
2020-08-11 23:05:17 +0530

    first

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="grid-container">
       <header>
-        <a href="/">Amruta Shopping</a>
+        <a href="/"> Shopping</a>
       </header>
       <main>Product List</main>
       <footer>All right is reserved.</footer>

95a84552c43463c55026c352106c4074127ede9a
Amruta Khot <amrutakhot2994@gmail.com>
2020-08-11 23:12:23 +0530

    second

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="grid-container">
       <header>
-        <a href="/"> Shopping</a>
+        <a href="/"> React Shopping</a>
       </header>
       <main>Product List</main>
       <footer>All right is reserved.</footer>
